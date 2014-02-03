e2b3161f571e4174b92e9bdfc3bdf01865394cab
Star Li <star.estella.li@gmail.com>
2014-02-02 18:04:15 -0800

    Configured app for Production emails

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app21586386@heroku.com'],
-    :password       => ENV['ulxl1po9'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
