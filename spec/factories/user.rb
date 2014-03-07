13e7a6ca8e7fa1407c2fbf3c1f59dbf065020f03
Star Li <star.estella.li@gmail.com>
2014-03-07 15:54:13 -0800

    Wrote test spec for User#top_rated

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
   factory :user do
     name "Douglas Adams"
-    email "douglas@example.com"
+    sequence(:email) { |n| "person#{n}@example.com" }
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
   end
 end
+
+#This will generate a unique email for each person, which is great because Devise has a validation to check email uniqueness.
