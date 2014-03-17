f49402dd1668c95d349c437d1bc3332583b63359
Star Li <star.estella.li@gmail.com>
2014-03-17 01:03:55 -0700

    Ajaxified comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,22 @@ class CommentsController < ApplicationController
     @topic = Topic.find(params[:topic_id])  ##finds the current topic by using the ID that's in the prameters and stores that in @topic
     @post = @topic.posts.find(params[:post_id])  ##this line finds the post based on the params, just like the line above, except within in the scope of topics
     #@post = Post.find(params[:post_id])  could also be written like this
+    @comments = @post.comments
 
     @comment = current_user.comments.build(params[:comment])   ##first line builds a comment in the context of the current_user and the params@comment.post = @post   ##second line adds the post_id to the comment (before you save)
     @comment.post = @post  ##second line adds the post_id to the comment (before you save)
-
+    @new_comment = Comment.new
+    
     authorize! :create, @comment, message: "You need be signed in to do that."
+
     if @comment.save
-      redirect_to [@topic, @post], notice: "Comment was saved successfully"
+      flash[:notice] = "Comment was created."
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render :new
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
     end
   end
 
